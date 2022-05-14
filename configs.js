0193ef83bb1f02968a4e3b037a0c8f3e61b2f365
Abuelo <chicoaaron200gmail.com>
2022-05-14 16:38:55 +0200

    first commit

---
+++ configs.js
@@ -0,0 +1,14 @@
+const configs = {
+    token: "NzE1MTU3NTA0MDE4ODc0NDA5.Xs5IUw.xATZcq1maPpbbj1pJxq48IrNCO4",
+    dbLink: "mongodb+srv://Bot:oFTBc0qWiBjDqr2i@cluster0.qtoot.mongodb.net/General?retryWrites=true&w=majority"
+};
+
+const owners = [
+    "887062134964498513",
+    "515649867385798656"
+];
+
+module.exports = {
+    configs,
+    owners
+};
